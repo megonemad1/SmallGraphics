27232909de06ec896ab4e9686131083928a4966e
Rhys Puddephatt <rhys.puddephatt@btinternet.com>
2017-02-04 19:52:33 +0000

    get wrecked

--- main.c
+++ main.c
@@ -30,9 +30,14 @@ void init(void) {
 
 	init_lcd();
 
-
-	display_string("hello world");
-
+display_string("\n\n           _                      \n");
+display_string("  __ _ ___| |_                    \n");
+display_string(" / _` / -_)  _|                   \n");
+display_string(" \\__, \\___|\\__|     _          _  \n");
+display_string(" |___/ ___ _ ___ __| |_____ __| | \n");
+display_string(" \\ V  V / '_/ -_) _| / / -_) _` | \n");
+display_string("  \\_/\\_/|_| \\___\\__|_\\_\\___\\__,_|");
+/*
 	drawCircle(160,120,100,YELLOW);
 	drawCircle(115, 75, 15,YELLOW);
 	drawCircle(205, 75, 15,YELLOW);
@@ -40,9 +45,47 @@ void init(void) {
 	uint16_t x[4] = {115, 135, 185, 205};
 	uint16_t y[4] = {165, 195, 195,165};
 	plotBezierBuffer(x,y, YELLOW, 0.1);
+*/
 
+	uint16_t l1x[4] = {50, 25, 25, 50};
+	uint16_t l1y[4] = {150, 175, 200, 225};
+	plotBezierBuffer(l1x,l1y, YELLOW, 0.1);
 
+	uint16_t l2x[4]  = {50, 75, 75, 80};
+	uint16_t l2y[4]  = {150, 150, 150, 155};
+	plotBezierBuffer(l2x,l2y, YELLOW, 0.1);
 
+    drawLine(80,155,80,90,YELLOW);
+    drawLine(110,155,110,90,YELLOW);
+    drawLine(106,100,84,100,YELLOW);
+
+    drawLine(84,90,84,100,YELLOW);
+    drawLine(106,90,106,100,YELLOW);
+
+	uint16_t l3x[4]  = {110, 120, 130, 140};
+	uint16_t l3y[4]  = {155, 145, 145, 155};
+	plotBezierBuffer(l3x,l3y,YELLOW, 0.1);
+
+	uint16_t l4x[4]  = {140, 150, 160, 170};
+	uint16_t l4y[4]  = {155, 145, 145, 155};
+	plotBezierBuffer(l4x,l4y,YELLOW, 0.1);
+
+	uint16_t l5x[4] = {170, 25+170, 25+170, 170};
+	uint16_t l5y[4] = {155, 175, 200, 225};
+	plotBezierBuffer(l5x,l5y, YELLOW, 0.1);
+
+	uint16_t l6x[4] = {80, 90, 100, 110};
+	uint16_t l6y[4] = {90, 60, 60, 90};
+	plotBezierBuffer(l6x,l6y, YELLOW, 0.1);
+
+	uint16_t l7x[4] = {84, 90, 100, 106};
+	uint16_t l7y[4] = {90, 64, 64, 90};
+	plotBezierBuffer(l7x,l7y, YELLOW, 0.1);
+
+	uint16_t l8x[4] = {50, 10, 15, 20};
+	uint16_t l8y[4] = {225, 200, 190, 175};
+	plotBezierBuffer(l8x,l8y, YELLOW, 0.1);
+    drawLine(20,175,34,171,YELLOW);
 }
 
 int main() {
