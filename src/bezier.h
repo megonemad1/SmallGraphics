e0d120b8dfacbfa4ec5390726d0f2985a7f0c50a
Rhys Puddephatt <rhys.puddephatt@btinternet.com>
2017-02-04 16:12:19 +0000

    combind the bres with the brz

--- src/bezier.h
+++ src/bezier.h
@@ -1,11 +1,14 @@
 #ifndef bezier_h
 #define bezier_h
 #include <avr/io.h>
-#include "bresenham.h"
-#include "bezier.h"
-#include <stdlib.h>
+#include <avr/interrupt.h>
 #include <math.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include "bezier.h"
 #include "unifiedColor.h"
+#include "ili934x.h"
+#include "unifiedLcd.h"
 
  /*{Small Graphics library for the LaFortuna}
     Copyright (C) {2016}  {Nicholas Bishop}
@@ -27,6 +30,11 @@
 supported.*/
 
 /* A library for drawing anti-aliased lines using wu's algorithm */
+
+/* The bresenham library provides an efficient way of drawing lines using the bresenham algorithm. The bresenham library
+also provides an efficient function for drawing circles, based off the midpoint circle algorithm. */
+
+
 
 /* Auxillary function for drawing bezier curves */
 int16_t findBezier(double t, uint16_t x[4]);
@@ -69,5 +77,44 @@ y1 - The y coordinate of the end point of the line
 col - The color of the line in svgrgb565 format*/
 void drawAALine(double x0, double y0, double x1, double y1, uint16_t col);
 
+
+/* Draws a straight line on the screen using bresenham's line algorithm 
+
+Parameters:
+x1 - The  coordinate of the start point of the line
+y1 - The y coordinate of the start point of the line
+x2 - The x coordinate of the end point of the line
+y2 - The y coordinate of the end point of the line
+col - The color of the line in svgrgb565 format
+*/
+void drawLine(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t col);
+
+/* Colors a pixel on the screen at the specified x and y coordinates with the specified color (in svgrgb565 format) 
+
+Parameters:
+x - The  coordinate of the pixel
+y - The y coordinate of the spixel
+col - The new color of the pixel in svgrgb565 format
+*/
+void plotPixel(uint16_t x, uint16_t y, uint16_t col);
+
+/*auxillary functions for drawing lines */
+uint16_t findOctant(int16_t x1, int16_t y1, int16_t x2, int16_t y2);
+void swap(int16_t *x, int16_t *y);
+void switchFromOctant0(int16_t *x, int16_t *y, uint16_t octant);
+void toOctant0(int16_t *x, int16_t *y, uint16_t octant);
+
+/*Draws a circle on the screen using the midpoint circle algorithm
+
+Parameters:
+xc - The x coordinate of the centre of the circle
+yc - The y coordinate of the centre of the circle
+r - The radius of the circle
+col - The color of the circle in svgrgb565 format
+*/
+void drawCircle(uint16_t xc, uint16_t yc, int32_t r, uint16_t col);
+
+/* auxillary function for drawing circles */
+void auxCircle(uint16_t xc, uint16_t yc, uint16_t x, uint16_t y, uint16_t col);
 
 #endif
